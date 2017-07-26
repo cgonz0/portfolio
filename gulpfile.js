fe89de3dfc55284739c7dcbacda71208833fe699
cgonz0 <cgonzadesign@gmail.com>
2017-07-26 12:12:01 -0700

    update layout + optimization
    new header, footer, content - plus concatenate, minifiy, optimize images, etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+// !Development Tasks
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync.init({
+    proxy: "http://localhost:8000",
+  })
+});
  
 gulp.task('sass', function () {
   return gulp.src('sass/**/*.scss')
@@ -10,17 +26,50 @@ gulp.task('sass', function () {
       stream: true
     })) 
 });
- 
+
+// Watchers
 gulp.task('watch', ['browserSync', 'sass'], function () {
   gulp.watch('sass/**/*.scss', ['sass']);
   gulp.watch('./*.html', browserSync.reload); 
   gulp.watch('./scripts/**/*.js', browserSync.reload); 
 });
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    proxy: "http://localhost:8000",
-  })
+
+// !Optimization Tasks
+// Optimizing CSS and JavaScript 
+gulp.task('useref', function(){
+  return gulp.src('./*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
 });
 
+// Optimizing Images 
+gulp.task('images', function(){
+  return gulp.src('./images/**/*.+(png|jpg|gif|svg)')
+  .pipe(cache(imagemin()))
+  .pipe(gulp.dest('dist/images'))
+});
+
+// Cleaning 
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+// !Build Sequences
+gulp.task('default', function (callback) {
+  runSequence(['sass', 'browserSync', 'watch'],
+    callback
+  )
+})
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+})
 
